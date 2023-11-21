6fb2cb07f0f7f0e9f6e5ecdf932315618d6fce4c
david kurman <dividiak@gmail.com>
2023-11-21 10:09:10 +0200

    4

--- dist/server.js
+++ dist/server.js
@@ -4,9 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.use('/file', express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use('/file', express_1.default.static('public'));
 app.get('/a', ((req, res) => {
     res.send('aaa');
 }));
